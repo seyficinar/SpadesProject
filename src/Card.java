73f6b477fbd9b12ff73fef634206e9e14c81a95a
xyz <xyz@10.200.109.205>
2023-03-21 11:51:05 +0300

    Card class

---
+++ src/Card.java
@@ -0,0 +1,33 @@
+
+public class Card {
+	
+	int value;
+	String type;
+	String face;
+	public Card(int value, String type, String face) {
+		super();
+		this.value = value;
+		this.type = type;
+		this.face = face;
+		
+		if(face.equals("Jack")) {
+			this.value = 11;
+		}
+		if(face.equals("Queen")) {
+			this.value = 12;
+		}
+		if(face.equals("King")) {
+			this.value = 13;
+		}
+		if(face.equals("Ace")) {
+			this.value = 14;
+		}
+	}
+	
+	
+	
+	
+	
+	
+
+}
